ea6e045ad62fa6b495440af327678a2b4328b2f7
marirs <marirs@gmail.com>
2022-02-20 15:41:33 +0530

    tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,7 @@
 use crate::CefToHashMap;
 
 #[test]
-fn test_not_cef_string() {
+fn test_non_cef_string() {
     let s = "this is not a cef string|key=value";
     assert!(s.to_hashmap(false).is_err())
 }
@@ -16,4 +16,44 @@ fn test_string_to_hashmap() {
 fn test_str_to_hashmap() {
     let s = "CEF:0|Vendor|Product|20.0.560|600|User Signed In|3|";
     assert!(s.to_hashmap(false).is_ok())
+}
+
+#[test]
+fn test_with_raw_event() {
+    let s = "CEF:0|Vendor|Product|20.0.560|600|User Signed In|3|";
+    let x = s.to_hashmap(true);
+    assert!(x.is_ok());
+    assert!(x.unwrap().get("rawEvent").is_some())
+}
+
+#[test]
+fn test_without_raw_event() {
+    let s = "CEF:0|Vendor|Product|20.0.560|600|User Signed In|3|";
+    let x = s.to_hashmap(false);
+    assert!(x.is_ok());
+    assert!(x.unwrap().get("rawEvent").is_none())
+}
+
+#[test]
+fn test_pri_facility() {
+    let s = "<134>CEF:0|Vendor|Product|20.0.560|600|User Signed In|3|";
+    let x = s.to_hashmap(false);
+    assert!(x.is_ok());
+    let x = x.unwrap();
+    assert!(x.get("syslogPriority").is_some());
+    assert!(x.get("syslogFacility").is_some());
+}
+
+#[test]
+fn test_cef_headers_exist() {
+    let s = "<134>CEF:0|Vendor|Product|20.0.560|600|User Signed In|3|";
+    let x = s.to_hashmap(false);
+    assert!(x.is_ok());
+    let x = x.unwrap();
+    assert!(x.get("deviceVendor").is_some());
+    assert!(x.get("deviceProduct").is_some());
+    assert!(x.get("deviceVersion").is_some());
+    assert!(x.get("signatureId").is_some());
+    assert!(x.get("name").is_some());
+    assert!(x.get("severity").is_some());
 }
