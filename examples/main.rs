fccc348f716a60d0e84aa22b162836ec96f006d4
marirs <marirs@gmail.com>
2022-02-20 10:56:28 +0530

    more examples

--- examples/main.rs
+++ examples/main.rs
@@ -1,6 +1,10 @@
 use cef2hashmap::CefToHashMap;
 
 fn main() {
-    let cef = "<134>2022-02-14T03:17:30-08:00 TEST CEF:0|Trend Micro|Deep Security Manager|20.0.560|600|User Signed In|3|src=135.181.193.110 suser=MasterAdmin target=MasterAdmin msg=User signed in from 135.181.193.110 TrendMicroDsTenant=Primary TrendMicroDsTenantId=0";
-    println!("{:#?}", cef.to_hashmap(true));
+    let examples = include_str!("examples.txt").trim();
+    for (i, eg) in examples.lines().enumerate() {
+        println!("--- Example Line #{} ---", i+1);
+        println!("{:#?}", eg.to_hashmap(true));
+        println!();
+    }
 }
